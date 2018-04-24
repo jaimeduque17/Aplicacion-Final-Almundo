4931ca216caf61964028dbf20ae7a1d66db2d3eb
Jaime Duque <jaime.duque@MacBook-Pro-de-Jaime-Duque.local>
2018-04-24 10:17:46 -0500

    se agrega funcionalidad a los botones solo ida, ida y vuelta y multidestino

--- src/Components/NavBar/Index.tsx
+++ src/Components/NavBar/Index.tsx
@@ -7,8 +7,13 @@ import { BoxShadow } from 'react-native-shadow'
 // import { initializeButtonAnimate } from '../../Helpers/Animated'
 import { getComponentStyle } from '../../Helpers/Stylus'
 import { Icon } from '../../Helpers/Icons'
+import { Actions } from 'react-native-router-flux'
 
 class NavBar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
 
     render() {
         const shadowOpt = {
@@ -22,33 +27,8 @@ class NavBar extends Component {
             y: 3,
             style: { marginBottom: 5 }
         }
-
-        // initializeButtonAnimate()
-        // let btnOrder: any
-        // let btnFilter: any
-        // let btnMap: any
-
-        // const handlerbtnOrder = ref => btnOrder = ref
-        // const handlerbtnFilter = ref => btnFilter = ref
-        // const handlerbtnMap = ref => btnMap = ref
-
-        // function order() {
-        //     btnOrder.zoomOut()
-        // }
-
-        // function filter() {
-        //     btnFilter.zoomOut()
-        // }
-
-        // function map() {
-        //     btnMap.zoomOut()
-        // }
         return (
             <BoxShadow setting={shadowOpt} >
-                {/* <LinearGradient
-                    colors={['rgb(239, 83, 117)', 'rgb(234, 100, 34)']}
-                    start={{ x: 0.0, y: 0.0 }} end={{ x: 1.0, y: 1.0 }}
-                    style={{ width: 360, height: 128 }}> */}
                     <View style={styles.containerCentral}>
                         <TouchableOpacity style={styles.btnBack}>
                             <Icon iconName='back' iconStyle={styles.iconBack} />
@@ -59,39 +39,20 @@ class NavBar extends Component {
                     </View>
                     <View style={styles.containerBtns}>
                         <TouchableOpacity style={styles.btnAnimation}
-                        // onPress={() => order()}
-                        >
-                            {/* <Animatable.View
-                                ref={handlerbtnOrder}
-                                duration={1000}
-                                style={styles.circle}>
-                            </Animatable.View> */}
+                        onPress={() => { Actions.flight() }}>
                             <Text style={styles.txtOrder}>{'SOLO IDA'}</Text>
                         </TouchableOpacity>
                         <View style={styles.breakBar1}></View>
                         <TouchableOpacity style={styles.btnAnimation}
-                        // onPress={() => filter()}
-                        >
-                            {/* <Animatable.View
-                                ref={handlerbtnFilter}
-                                duration={1000}
-                                style={styles.circle}>
-                            </Animatable.View> */}
+                        onPress={() => { Actions.flightRound() }}>
                             <Text style={styles.txtFilter}>{'IDA Y VUELTA'}</Text>
                         </TouchableOpacity>
                         <View style={styles.breakBar2}></View>
                         <TouchableOpacity style={styles.btnAnimation}
-                        // onPress={() => map()}
-                        >
-                            {/* <Animatable.View
-                                ref={handlerbtnMap}
-                                duration={1000}
-                                style={styles.circle}>
-                            </Animatable.View> */}
+                        onPress={() => { Actions.flightMulti() }}>
                             <Text style={styles.txtMap}>{'MULTIDESTINO'}</Text>
                         </TouchableOpacity>
                     </View>
-                {/* </LinearGradient> */}
             </BoxShadow>
         )
     }
